27242e6e4bd682e828caa05ee0acc1e9155bb608
Jinhyun Park <developerjhp@gmail.com>
2024-01-25 21:27:34 +0900

    Time: 60 ms (24.68%) | Memory: 50 MB (5.24%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -7,7 +7,7 @@ var mergeAlternately = function (word1, word2) {
     let res = ''
     const most = Math.max(word1.length, word2.length)
 
-    for (let i = 0; i <= most; i++) {
+    for (let i = 0; i < most; i++) {
         res += word1[i] ?? ''
         res += word2[i] ?? ''
     }
