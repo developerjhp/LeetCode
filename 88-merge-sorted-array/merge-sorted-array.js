35207e522182680cfae05573d559d90e9e66613c
Jinhyun Park <developerjhp@gmail.com>
2024-01-25 21:42:21 +0900

    Time: 52 ms (65.22%) | Memory: 48.5 MB (7.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -9,12 +9,8 @@ var merge = function(nums1, m, nums2, n) {
     for (let i=0; i<n; i++) {
         nums1.pop()
     }
-
     for (let j=0; j<n; j++) {
         nums1.push(nums2[j])
     }
-
-
-
     nums1.sort((a,b) => a-b)
 };
