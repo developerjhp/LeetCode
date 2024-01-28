8764fb81b5babf2293cf4e2daf6b0fde00369a02
Jinhyun Park <developerjhp@gmail.com>
2024-01-28 14:48:48 +0900

    Time: 55 ms (83.65%) | Memory: 51.1 MB (5.07%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,10 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for (let i=0; i <nums.length; i++) {
-        for (let j =i+1; j<nums.length; j++) {
-            if (nums[j] + nums[i] === target) return [i,j]
+var twoSum = function (nums, target) {
+    const numMap = new Map();
+    const len = nums.length
+    
+    for (let i = 0; i < len; i++) {
+        const comp = target - nums[i]
+
+        if (numMap.has(comp)) {
+            return [numMap.get(comp), i];
         }
-    } 
-}; 
+
+        numMap.set(nums[i], i)
+    }
+
+    return []
+};
